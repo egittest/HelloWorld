2427a2225939900957bbec20fbc02f692f0e04a0
zhujk <zhujk.icode@gmail.com>
2014-02-14 10:39:43 +0800

    add mul method.

--- src/com/flyingh/demo/Hello.java
+++ src/com/flyingh/demo/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!!");
 		System.out.println(add(1, 1));
+		System.out.println(mul(2, 3));
+	}
+
+	public static int mul(int a, int b) {
+		return a * b;
 	}
 
 	private static int add(int a, int b) {
