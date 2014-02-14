03fb8ec333afa36092ccce5f829b3316c53ffd71
zhujk <zhujk.icode@gmail.com>
2014-02-14 13:45:03 +0800

    add div method.

--- src/com/flyingh/demo/Hello.java
+++ src/com/flyingh/demo/Hello.java
@@ -18,4 +18,11 @@ public class Hello {
 	private static int add(int a, int b) {
 		return a + b;
 	}
+
+	public static int div(int a, int b) {
+		if (b == 0) {
+			throw new ArithmeticException();
+		}
+		return a / b;
+	}
 }
