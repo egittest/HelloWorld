5b8f44643991f04415005d36138454e2e50b632c
zhujk <zhujk.icode@gmail.com>
2014-02-14 15:30:41 +0800

    print CA.

--- src/com/flyingh/demo/Conflict.java
+++ src/com/flyingh/demo/Conflict.java
@@ -4,6 +4,7 @@ public class Conflict {
 	public static void main(String[] args) {
 		System.out.println("ca");
 		System.out.println("cb");
+		System.out.println("CA");
 		System.out.println("CB");
 	}
 }
