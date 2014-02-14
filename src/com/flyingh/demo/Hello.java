c6ea0330ddfe033f18b325c4afa3b87219055fff
zhujk <zhujk.icode@gmail.com>
2014-02-14 09:56:35 +0800

    add add method.

--- src/com/flyingh/demo/Hello.java
+++ src/com/flyingh/demo/Hello.java
@@ -3,5 +3,10 @@ package com.flyingh.demo;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!!");
+		System.out.println(add(1, 1));
+	}
+
+	private static int add(int a, int b) {
+		return a + b;
 	}
 }
