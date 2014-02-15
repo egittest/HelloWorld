cdb25dbb46fe5f3823c558a660d6e90b412102f6
zhujk <zhujk.icode@gmail.com>
2014-02-15 14:21:32 +0800

    print another message.

--- src/com/flyingh/demo/PushToFile.java
+++ src/com/flyingh/demo/PushToFile.java
@@ -3,5 +3,6 @@ package com.flyingh.demo;
 public class PushToFile {
 	public static void main(String[] args) {
 		System.out.println("PushToFile.main()");
+		System.out.println("PushToFile.main()");
 	}
 }
